89b3cce83851f6641f34c7eeaf08bbe140098949
Felipe Araujo <felipe.araujo.s@live.com>
2020-01-03 16:44:02 -0300

    odd squares

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,7 +4,21 @@ defmodule Identicon do
       |> hash_input
       |> pick_color
       |> build_grid
+      |> filter_odd_squares
+      #|> buid_pixel_map
   end
+
+  def buid_pixel_map(%Identicon.Image{grid: grid = image}) do
+
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) -> 
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid} 
+  end 
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
     grid =
